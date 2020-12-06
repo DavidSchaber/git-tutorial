8f854e29d880aae17d697aedc6bf1273b8349d23
Alex Hoffman <alxhoff@gmail.com>
2020-12-06 01:18:04 +0100

    coding challenge2 nearly working

--- src/hello_espl.c
+++ src/hello_espl.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <espl_lib.h>
 
-int main() {
-	printf("Hello ESPL\n");
-	printf("Test estest L\n");
+int main()
+{
+	int running = 1;
+	int test; 
+	int number;
+	char* x;
+	while (running != 0)
+	{
+		
+		printf("Enter your number of wish below!\n");
+		
+		scanf("%i ", &number);
+
+		x = num_to_words(number);
+		
+		printf("This is the converted Number you wrote: %s\n", x);
+
+		printf("If you wish to continue enter 1 (or any other number except 0) or if you wish to exit enter 0\n");
+		scanf("%i ", &test);
+		if (test == 0)
+		{
+			running = 0;
+		}
+		
+
+	}
+	printf("Thank you for working with this programm\n");
 	return 0;
 }
